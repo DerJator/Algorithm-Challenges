739b3ec891d334a1e3c601f847c047f73ed757cf
Jonathan Stahlberger <jonathan.stahlberger@gmail.com>
2024-04-30 10:10:41 +0200

    Rewrite without numpy

--- HW-Algorithms/TopSecret/TopSecret-MASTER.py
+++ HW-Algorithms/TopSecret/TopSecret-MASTER.py
@@ -1,30 +1,66 @@
-import numpy as np
 import sys
+import numpy as np
+
+
+def mat_vec_mult(A, v):
+    result = [0 for _ in range(len(A))]
+
+    for i in range(len(A)):
+        result[i] = sum([A[i][j] * v[j] for j in range(len(A[0]))])
+
+    return result
+
+def matrix_multiply(A, B):
+    """ Multiply two matrices. """
+    rows_A = len(A)
+    cols_A = len(A[0])
+    if not type(B[0]) == int:
+        cols_B = len(B[0])
+    else:
+        return mat_vec_mult(A, B)
+
+    result = [[0 for _ in range(cols_B)] for _ in range(rows_A)]
+
+    for i in range(rows_A):
+        for j in range(cols_B):
+            for k in range(cols_A):
+                result[i][j] += A[i][k] * B[k][j]
+
+    return result
+
+
+def identity_matrix(size):
+    """ Generate identity matrix of given size. """
+    return [[1 if i == j else 0 for j in range(size)] for i in range(size)]
+
 
 def fast_exp(A, n: int, thresh: int):
     """ Recursively multiply squared matrix A until exponent n is reached. Keep numbers lower than 10^thresh. """
-    a_dim = A.shape[0]
     if n == 0:
-        return np.identity(a_dim, dtype=int)
-    res = fast_exp(A, int(n/2), thresh) % (10 ** thresh)
-    R = np.dot(res, res)
+        return identity_matrix(len(A))
+
+    res = fast_exp(A, n // 2, thresh)
+    res = [[elem % (10 ** thresh) for elem in row] for row in res]  # Only take thresh last digits to avoid huge numbers
+    R = matrix_multiply(res, res)
+
     if n % 2 == 1:
-        return np.dot(R, A)
+        R = matrix_multiply(R, A)
+
     return R
 
 def parse_txt(input: str):
     lines = input.split('\n')
     no_samples = eval(lines.pop(0))
-    params = np.zeros((no_samples, 5), dtype=int)
+    params = []
     numbers = []
 
-    for i in range(no_samples):
-        params[i] = [eval(s) for s in lines.pop(0).split(" ")]
-        n_vals = [eval(s) for s in lines.pop(0).split(" ")]
-        print(lines.pop(0))
+    for _ in range(no_samples):
+        param_values = [int(s) for s in lines.pop(0).split(" ")]
+        params.append(param_values)
+        n_vals = [int(s) for s in lines.pop(0).split(" ")]
+        _ = lines.pop(0)  # Pop the empty line
 
         numbers.append(n_vals)
-
 
     return no_samples, params, numbers
 
@@ -41,28 +77,28 @@ if __name__ == '__main__':
         # print(f"{N_in=}")
 
         """ Build matrix """
-        a_mat = np.zeros((N, N), dtype=int)
+        a_mat = [[0] * N for _ in range(N)]
 
         # First row
-        a_mat[0, N-1] = L
-        a_mat[0, 0] = 1
-        a_mat[0, 1] = R
+        a_mat[0][N - 1] = L
+        a_mat[0][0] = 1
+        a_mat[0][1] = R
 
-        # in-between rows
+        # In-between rows
         for i in range(1, N - 1):
-            a_mat[i, (i-1):(i+2)] = [L, 1, R]
+            a_mat[i][i - 1] = L
+            a_mat[i][i] = 1
+            a_mat[i][i + 1] = R
 
         # Last row
-        a_mat[N-1, N-2] = L
-        a_mat[N-1, N-1] = 1
-        a_mat[N-1, 0] = R
+        a_mat[N - 1][N - 2] = L
+        a_mat[N - 1][N - 1] = 1
+        a_mat[N - 1][0] = R
 
-        """ The calculation """
-
-        run_N = np.array(N_in, dtype=int)
-        run_N = fast_exp(a_mat, S, X) @ run_N
+        run_N = N_in[:]
+        run_N = matrix_multiply(fast_exp(a_mat, S, X), run_N)
 
         with open("1.ans", "a") as file:
-            file.write(' '.join(map(str, run_N % (10 ** X))))
+            file.write(' '.join(map(str, [elem % (10 ** X) for elem in run_N])))
             if not j == no_samples - 1:
                 file.write("\n")

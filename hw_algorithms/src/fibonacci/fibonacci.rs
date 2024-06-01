1152bd1e04581f6a65474958455242e35269bdbe
Jonathan Stahlberger <jonathan.stahlberger@fau.de>
2024-06-01 22:04:28 +0200

    Clean up fibonacci

--- hw_algorithms/src/fibonacci/fibonacci.rs
+++ hw_algorithms/src/fibonacci/fibonacci.rs
@@ -1,5 +1,4 @@
 use std::io;
-use std::io::{Lines, StdinLock};
 use crate::helpers;
 
 
@@ -13,9 +12,10 @@ pub fn main() {
     let mut line_val = String::new();
     let mut params: Vec<u64>;
 
-    let mut fib = vec![vec![0;2];2];
-    let fib_start = vec![1;2];
-    let fib_matrix = vec![vec![1, 1], vec![1, 0]];
+    //Initialize important values
+    let mut fib = vec![vec![0;2];2]; // saves exp. fib matrix
+    let fib_start = vec![1;2]; // starting values F_0 and F_1
+    let fib_matrix = vec![vec![1, 1], vec![1, 0]]; // Fibonacci iteration matrix
 
     for _ in 0..n_cases {
         // params: j m
@@ -29,38 +29,15 @@ pub fn main() {
         let modulo_op = params[1];
 
         if n_iter > 1 {
-            // S>1: Do algorithm
-            fast_exp(&mut fib, &fib_matrix, n_iter, modulo_op);
+            // n > 1: Do algorithm
+            helpers::fast_exp(&mut fib, &fib_matrix, n_iter, modulo_op);
         } else if n_iter <= 1 {
             println!("{}", 1);
             continue
         }
 
         let final_res = helpers::mat_vec_mult(&fib, &fib_start, modulo_op);
-        // print!("Result vec: ");
         println!("{}", final_res[1]);
-
     }
 }
-
-
-fn fast_exp(new_fib: &mut Vec<Vec<u64>>, fib_mat: &Vec<Vec<u64>>, n: u64, thresh: u64) {
-    // Recursively multiply squared matrix A until exponent n is reached. Keep numbers lower than thresh
-    // Runtime: log(s) * n^3
-
-    // println!("n: {}", n);
-    if n == 0 {
-        *new_fib = vec![vec![0, 1], vec![1, 0]];
-        return;
-    }
-
-    fast_exp(new_fib, fib_mat, n / 2, thresh);
-
-    if n % 2 == 0 {
-        *new_fib = helpers::matrix_multiply(new_fib, new_fib, thresh);
-    } else {
-        *new_fib = helpers::matrix_multiply(fib_mat, &helpers::matrix_multiply(new_fib, new_fib, thresh), thresh);
-    }
-}
-
 
